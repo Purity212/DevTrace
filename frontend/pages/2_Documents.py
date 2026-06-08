13657546b4ab4e00db869b96f0abbe450c272bff
asprimak <andrey40rus2002@mail.ru>
2026-06-08 19:27:28 +0300

    fix: fix Docs page

--- frontend/pages/2_Documents.py
+++ frontend/pages/2_Documents.py
@@ -1,43 +1,56 @@
+import requests
 import streamlit as st
 
 
 st.set_page_config(page_title="DevTrace | Documents", page_icon="DT", layout="wide")
 
+API_BASE = "http://127.0.0.1:8000" # для общения с fastapi
 
-DEFAULT_PROJECTS = [
-    {"id": 2, "name": "X_eng", "description": "Y_eng"},
-    {"id": 1, "name": "X", "description": "X"},
-]
+def get_documents():
+    response = requests.get(f"{API_BASE}/documents", timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+def create_document(document_type: str, file):
+    input = {"document_type": document_type, "file": file}
+    response = requests.post(f"{API_BASE}/documents", json=input, timeout=10)
+    return response.json()
+
+
+# DEFAULT_PROJECTS = [
+#     {"id": 2, "name": "X_eng", "description": "Y_eng"},
+#     {"id": 1, "name": "X", "description": "X"},
+# ]
 
 # GET /api/projects/{project_id}/documents
-DEFAULT_DOCUMENTS = [
-    {
-        "id": 2,
-        "project_id": 2,
-        "filename": "flight_requirements_v1.docx",
-        "document_type": "requirement",
-    },
-    {
-        "id": 1,
-        "project_id": 2,
-        "filename": "autopilot_controller.cpp",
-        "document_type": "source_code",
-    },
-]
+# DEFAULT_DOCUMENTS = [
+#     {
+#         "id": 2,
+#         "project_id": 2,
+#         "filename": "flight_requirements_v1.docx",
+#         "document_type": "requirement",
+#     },
+#     {
+#         "id": 1,
+#         "project_id": 2,
+#         "filename": "autopilot_controller.cpp",
+#         "document_type": "source_code",
+#     },
+# ]
 
 
-if "projects_demo_data" not in st.session_state:
-    st.session_state.projects_demo_data = DEFAULT_PROJECTS.copy()
+# if "projects_demo_data" not in st.session_state:
+#     st.session_state.projects_demo_data = DEFAULT_PROJECTS.copy()
 
-if "documents_demo_data" not in st.session_state:
-    st.session_state.documents_demo_data = DEFAULT_DOCUMENTS.copy()
+# if "documents_demo_data" not in st.session_state:
+#     st.session_state.documents_demo_data = DEFAULT_DOCUMENTS.copy()
 
 
 st.title("Documents")
 st.write("Болванка страницы документов без привязки к API.")
 
 projects = st.session_state.projects_demo_data
-documents = st.session_state.documents_demo_data
+#documents = st.session_state.documents_demo_data
 
 if not projects:
     st.warning("Сначала создайте проект на вкладке Projects.")
@@ -93,6 +106,6 @@ st.dataframe(
     hide_index=True,
 )
 
-if st.button("Сбросить демо-документы"):
-    st.session_state.documents_demo_data = DEFAULT_DOCUMENTS.copy()
-    st.info("Демо-список документов восстановлен.")
+# if st.button("Сбросить демо-документы"):
+#     st.session_state.documents_demo_data = DEFAULT_DOCUMENTS.copy()
+#     st.info("Демо-список документов восстановлен.")
